99a38b207e1a0d7ca52e5ccf34b21dc3563ad838
Sunidhi Pandey <sunidhi.pandey@bksd.com>
2022-01-04 11:33:40 +0530

    adding activity to lib and uncommenting manifest

---
+++ MyCoollib/src/main/java/com/example/mycoollib/TestActivity.java
@@ -0,0 +1,41 @@
+package com.example.mycoollib;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TestActivity  extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+//        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+       /* AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();*/
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+//        NavigationUI.setupWithNavController(binding.navView, navController);
+
+        setContentView(R.layout.activity_main);
+
+        Point buenosAiresObeliscoPoint =
+                new Point((float) -34.6037389, (float) -58.3815704);
+
+        Point nycStatueOfLibertyPoint =
+                new Point((float) 40.6892494, (float) -74.0445004);
+
+        float distanceBetweenPoints = LatLonDistanceCalculator.calculateDistance(
+                buenosAiresObeliscoPoint,
+                nycStatueOfLibertyPoint);
+
+        TextView kilometersLabel = (TextView) findViewById(R.id.kilometers_label);
+
+        kilometersLabel.setText("Between the Obelisco and the Statue of Liberty there are: \n" + distanceBetweenPoints + "KM");
+    }
+}
